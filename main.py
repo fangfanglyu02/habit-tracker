e7d6b5529d886fc71bf7942d651018b27b36a973
FangfangLyu <adaisy.lyu@gmail.com>
2026-05-18 07:40:12 -0400

    feat: complete simple full-stack habit tracker with CRUD and persistent database

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import Habit, get_db
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/")
-def home():
-	return {"message": "Habit Tracker is running"}
+def root():
+	return {"message": "Habit Tracker with Database"}
 
-@app.get('/hello')
-def say_hello():
-	return {"text": "Hi, SWE"}
+@app.get('/habits')
+def get_habits(db: Session = Depends(get_db)):
+	habits = db.query(Habit).all()
+	return habits
+
+@app.post("/habits")
+def create_habit(name: str, db: Session = Depends(get_db)):
+	new_habit = Habit(name=name, done = False )
+	db.add(new_habit)
+	db.commit()
+	db.refresh(new_habit)
+	return new_habit
+
+@app.delete("/habits/{habit_id}")
+def delete_habit(habit_id:int, db: Session = Depends(get_db)):
+	habit = db.query(Habit).filter(Habit.id == habit_id).first()
+	if not habit:
+		raise HTTPException(status_code = 404, detail = "Habit not found")
+	db.delete(habit)
+	db.commit()
+	return {"message":"deleted"}
+
+@app.put("/habits/{habit_id}/toggle")
+def toggle_habit(habit_id:int, db:Session = Depends(get_db)):
+	habit = db.query(Habit).filter(Habit.id == habit_id).first()
+	if not habit:
+		raise HTTPException(status_code = 404, detail = "Habit not found")
+	habit.done = not habit.done
+	db.commit()
+	return habit
